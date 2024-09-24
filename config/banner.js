9eddf8b85ea8525650e8be0e194cb833294818d8
Kaguya Shinomiya <kunciran1st@gmail.com>
2024-09-25 05:04:41 +0700

    Create banner.js

---
+++ config/banner.js
@@ -0,0 +1,7 @@
+function printBanner() {
+  console.log("======== Coinsweeper Auto Play Game ========");
+  console.log("GitHub: https://github.com/kaguya1st");
+  console.log("Telegram: https://t.me/Pumpbtcxyz");
+  console.log("============================================");
+}
+module.exports = printBanner;
